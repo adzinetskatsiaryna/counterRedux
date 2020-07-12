49e299d537618504c05aa233bd52808eee219f6c
adzinets <adzinetskatsiaryna@gmail.com>
2020-07-12 14:18:57 +0300

    Updates

---
+++ precache-manifest.62847cfd1c1d74076ac23fbd8abfaed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951696b5af253a2559342f78a0471a5e",
+    "url": "/counterRedux/index.html"
+  },
+  {
+    "revision": "8112a923600560a09d51",
+    "url": "/counterRedux/static/css/main.30488faa.chunk.css"
+  },
+  {
+    "revision": "4b4db143063449f8c881",
+    "url": "/counterRedux/static/js/2.28be5870.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counterRedux/static/js/2.28be5870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8112a923600560a09d51",
+    "url": "/counterRedux/static/js/main.3713c9ca.chunk.js"
+  },
+  {
+    "revision": "b488f400b7f064eacb76",
+    "url": "/counterRedux/static/js/runtime-main.19dfb1b3.js"
+  }
+]);
